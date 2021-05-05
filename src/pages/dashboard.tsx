5c82c263bf8506d33e4faa588d224d0831398bbd
Gustavo Silva <gstvds@icloud.com>
2021-05-04 22:33:07 -0300

    fix: Changes dashboard to be responsive

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -63,52 +63,28 @@ const series = [
 
 export default function Dashboard() {
   return (
-    <Flex
-      direction="column"
-      h="100vh"
-    >
+    <Flex direction="column" h="100vh">
       <Header />
 
-      <Flex
-        w="100%"
-        my="6"
-        maxWidth={1480}
-        mx="auto"
-        px="6"
-      >
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <SimpleGrid
-          flex="1"
-          gap="4"
-          minChildWidth="320px"
-          align="flex-start"
-        >
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
           <Box
-            p="8"
+            p={[6, 8]}
             bg="gray.800"
             borderRadius={8}
           >
             <Text fontSize="lg" mb="4">Inscritos da semana</Text>
-            <Chart
-              type="area"
-              height={160}
-              options={options}
-              series={series}
-            />
+            <Chart type="area" height={160} options={options} series={series} />
           </Box>
           <Box
-            p="8"
+            p={[6, 8]}
             bg="gray.800"
             borderRadius={8}
           >
             <Text fontSize="lg" mb="4">Taxa de abertura</Text>
-            <Chart
-              type="area"
-              height={160}
-              options={options}
-              series={series}
-            />
+            <Chart type="area" height={160} options={options} series={series} />
           </Box>
         </SimpleGrid>
       </Flex>
